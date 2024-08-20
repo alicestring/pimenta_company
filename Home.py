33175c063d542b4ab99cad6dfcf8d4313f3add5f
alicestring <74160880+alicestring@users.noreply.github.com>
2024-08-20 03:46:06 -0300

    Update Home.py

--- Home.py
+++ Home.py
@@ -3,8 +3,6 @@ from PIL import Image
 
 st.set_page_config(page_title = 'Home', page_icon = '🤗', layout = 'wide')
 
-#image_path = 'C:\Users\solfm\repos\comunidadeds\FTC_Analisando_dados_com_python\'
-#image = Image.open(image_path + 'logo.png')
 image = Image.open('logo.png')
 st.sidebar.image(image, width = 120)
 
